7e07e74dcdc82d8fc86953d51ca7a46ac45583da
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-11-14 20:11:26 -0300

    style: add initial styling for page layout

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,22 @@
+import { Heading, Text } from "@ignite-ui/react"
+import Image from 'next/image'
+import { Container, Hero, Preview } from "./styles"
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendario e permitra que as pessoas marquem agendamentos 
+          no seu tempo livre
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image src={previewImage} height={400} quality={100} priority alt="preview" />
+      </Preview>
+    </Container>
+  )
+}
